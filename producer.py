9fc9bcb301bd1a4bb5f2023a4f87e27d59486ec4
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-11-19 17:53:16 -0800

    modify producer code

--- producer.py
+++ producer.py
@@ -1,10 +1,15 @@
 from kafka import KafkaProducer
 import time
 
-producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'])
+producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'], acks="all")
 iid = 0
 
 while True:
-    producer.send('foo', str.encode(f"{time.time()}: message {iid} from producer!"))
+    # message to deliver
+    msg = f"{time.time()}: message {iid} from producer!"
+    producer.send('foo', str.encode(msg))
+
+    print(f"Produced {msg}")
+    
     iid += 1
     time.sleep(1)
