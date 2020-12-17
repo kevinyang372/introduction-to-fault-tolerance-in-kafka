0741d63312fa9fd8bed063bea1054f153ba8f4cf
Yang Yunfan <yunfan.yang@minerva.kgi.edu>
2020-12-16 19:08:08 -0800

    update consumer and producer code

--- consumer.py
+++ consumer.py
@@ -2,4 +2,4 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('foo')
 for msg in consumer:
-    print(f"Consumed message from producer: {msg}")
+    print(f"Consumed message from partition {msg.partition}: {msg.value.decode('utf-8')}")
--- producer.py
+++ producer.py
@@ -1,15 +1,24 @@
 from kafka import KafkaProducer
+import random
 import time
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092', 'localhost:9093', 'localhost:9094'], acks="all")
+
 iid = 0
+available_keys = [b"foo_1", b"foo_2", b"foo_3", b"foo_4", b"foo_5"]
 
 while True:
-    # message to deliver
-    msg = f"{time.time()}: message {iid} from producer!"
-    producer.send('foo', str.encode(msg))
+    use_key = random.choice(available_keys)  # Choose a random key.
 
-    print(f"Produced {msg}")
-    
+    # Message to deliver.
+    msg = f"{time.time()}: message {iid} with key {use_key.decode('utf-8')} from producer!"
+    confirmation = producer.send('foo', value=str.encode(msg), key=use_key)
+
+    try:
+        confirmation.get()
+        print(f"Successfully produced {msg}!")
+    except Exception:
+        print(f"Failed to deliver {msg}")
+
     iid += 1
     time.sleep(1)
